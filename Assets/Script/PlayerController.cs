735391d1d89578cff6a48b10c2ff101858e5ca32
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-08-05 20:33:40 +0200

    first player move

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -42,5 +42,6 @@ public class PlayerController : MonoBehaviour
     void PlayerMove()
     {
         moveX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(moveX, 0f, 0f) * moveSpeed * Time.deltaTime;
     }
 }
